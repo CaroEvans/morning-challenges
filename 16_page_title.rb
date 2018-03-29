45532bfc1849cb38cd5c1cdb79b1d0185f4e5248
Caro Evans <caroline.w.evans@gmail.com>
2018-03-29 10:38:56 +1000

    solution to page title challenge

---
+++ 16_page_title.rb
@@ -0,0 +1,32 @@
+#
+# Page Title
+#
+# Write a method that takes a URL and
+# returns the website's title.
+#
+# Examples:
+# pageTitle("http://google.com") => "Google"
+# pageTitle("http://www.reddit.com") => "reddit: the front page of the internet"
+#
+# Check your answers by running the tests:
+# ruby tests/16_page_title_test.rb
+#
+# require 'metainspector'
+#
+# def pageTitle(url)
+#   title = []
+#   page = MetaInspector.new(url)
+#   title = page.title
+#   puts title
+# end
+#
+# pageTitle('google.com')
+
+
+
+
+require 'metainspector'
+
+def pageTitle(url)
+  puts MetaInspector.new(url).title
+end
