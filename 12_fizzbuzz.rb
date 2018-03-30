0511b53dd9376fcae535145c06186dc0ec9f35ee
Caro Evans <caroline.w.evans@gmail.com>
2018-03-30 22:18:19 +1000

    added github answers

--- 12_fizzbuzz.rb
+++ 12_fizzbuzz.rb
@@ -76,3 +76,20 @@ end
 #   return arr[-1]
 # end
 #
+# def fizz_buzz(max)
+#   arr = []
+#   (1..max).each do |n|
+#     text = ""
+#     if (n % 3 == 0)
+#       text << "Fizz"
+#     end
+#     if (n % 5 == 0)
+#       text << "Buzz"
+#     end
+#     if !((n % 3 == 0) || (n % 5 == 0))
+#       text = n
+#     end
+#     arr << text
+#   end
+#   return arr
+# end
