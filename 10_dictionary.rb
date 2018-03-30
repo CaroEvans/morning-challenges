62951c48a4a6fe50df588d9d810b9c78603d913a
Caro Evans <caroline.w.evans@gmail.com>
2018-03-30 20:57:11 +1000

    solution to dictionary challenge

--- 10_dictionary.rb
+++ 10_dictionary.rb
@@ -3,9 +3,9 @@
 # Extend the Dictionary class.
 #
 # It should have three methods:
-.add_word(word, definition) -> Should store a word and definition
-.lookup(word) -> Should return a definition
-.total_words -> Should return the number of words stored
+# .add_word(word, definition) -> Should store a word and definition
+# .lookup(word) -> Should return a definition
+# .total_words -> Should return the number of words stored
 #
 # Difficulty:
 # 4/10
@@ -20,7 +20,29 @@
 # Check your solution by running the tests:
 # ruby tests/10_dictionary_test.rb
 #
-
 class Dictionary
-  # Your code here
+  def initialize
+    @dictionary = []
+  end
+  def add_word(x, y)
+    @dictionary.push({word:x, definition:y})
+  end
+  def lookup(word)
+    @dictionary.each do |c|
+      if c[:word] == word
+        return c[:definition]
+      end
+    end
+  end
+  def total_words
+    count = 0
+    @dictionary.each do |c|
+    count += 1
+    end
+    return count
+  end
 end
+# dictionary = Dictionary.new
+# dictionary.add_word("ruby", "A precious stone")
+# dictionary.lookup("ruby")
+# dictionary.total_words
