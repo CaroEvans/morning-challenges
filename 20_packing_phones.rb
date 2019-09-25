d291930c4ec8e0dda57c6b5d53ce8de32519375c
Caro Evans <caroline.w.evans@gmail.com>
2019-09-26 08:07:31 +1000

    found 168

--- 20_packing_phones.rb
+++ 20_packing_phones.rb
@@ -19,11 +19,12 @@ total_combos.each do |combo|
   layer_info = {}
   layer_info[:box_height] = combo[1][2]
   layer_info[:phone_height] = combo[0][2]
-  layer_info[:phones_in_layer] = (combo[1][0]/combo[0][0]).floor*(combo[1][1]/combo[0][1]).floor* (combo[0][2]).floor
+  layer_info[:dimensions] = combo[0][0],combo[0][1],combo[0][2],combo[1][0],combo[1][1],combo[1][2]
+  no_in_layer = (combo[1][0]/combo[0][0]).floor * (combo[1][1]/combo[0][1]).floor
+  layer_info[:phones_in_layer] = no_in_layer
+  layer_info[:ratio] = (no_in_layer/combo[0][2])
   layer_vols.push(layer_info)
 end
-
-# puts layer_vols
 
 box_sides = [32.0, 43.0, 22.1]
 
@@ -31,50 +32,20 @@ grouped_by_box_side = []
 
 box_sides.each do |side|
   new_grouping = {}
-  new_grouping[:layers] = []
+  new_grouping[:"#{side}"] = []
   layer_vols.each do |layer|
-    layer[:box_height] == side ? new_grouping[:layers].push(layer) : next
+    layer[:box_height] == side ? new_grouping[:"#{side}"].push(layer) : next
   end 
   grouped_by_box_side.push(new_grouping)
 end
 
-pp grouped_by_box_side
 
-# orientations = phone_dimensions.permutation.to_a
+puts grouped_by_box_side[0][:"32.0"]
+puts '......'
+puts grouped_by_box_side[1][:"43.0"]
+puts '......'
+puts grouped_by_box_side[2][:"22.1"]
 
-# puts box_volume
-
-# _1_layer_volume = (b[:w]/orientations[0][0]).floor*(b[:l]/orientations[0][1]).floor*orientations[0][2].floor
-# _2_layer_volume =  (b[:w]/orientations[1][0]).floor*(b[:l]/orientations[1][1]).floor*orientations[1][2].floor
-# _3_layer_volume =  (b[:w]/orientations[2][0]).floor*(b[:l]/orientations[2][1]).floor*orientations[2][2].floor
-# _4_layer_volume =  (b[:w]/orientations[3][0]).floor*(b[:l]/orientations[3][1]).floor*orientations[3][2].floor
-# _5_layer_volume =  (b[:w]/orientations[4][0]).floor*(b[:l]/orientations[4][1]).floor*orientations[4][2].floor
-# _6_layer_volume =  (b[:w]/orientations[5][0]).floor*(b[:l]/orientations[5][1]).floor*orientations[5][2].floor
-
-# puts _1_layer_volume
-# puts _2_layer_volume
-# puts _3_layer_volume
-# puts _4_layer_volume
-# puts _5_layer_volume
-# puts _6_layer_volume
-
-
-
-
-# formulas = [
-
-#   (b[:w]/p[:w]).floor*(b[:l]/p[:l]).floor*(b[:d]/p[:d]).floor,
-  
-#   (b[:w]/p[:w]).floor*(b[:l]/p[:d]).floor*(b[:d]/p[:l]).floor,
-  
-#   (b[:w]/p[:d]).floor*(b[:l]/p[:l]).floor*(b[:d]/p[:w]).floor,
-  
-#   (b[:w]/p[:l]).floor*(b[:l]/p[:w]).floor*(b[:d]/p[:d]).floor,
-  
-#   (b[:w]/p[:l]).floor*(b[:l]/p[:d]).floor*(b[:d]/p[:w]).floor,
-  
-#   (b[:w]/p[:d]).floor*(b[:l]/p[:w]).floor*(b[:d]/p[:l]).floor
-#   ]
-
-
-
+# (64*2) + 40 = 168
+# 2 * {:box_height=>22.1, :phone_height=>7.4, :dimensions=>[4.0, 5.0, 7.4, 32.0, 43.0, 22.1], :phones_in_layer=>64, :ratio=>8.648648648648647}
+# 1 * {:box_height=>22.1, :phone_height=>5.0, :dimensions=>[4.0, 7.4, 5.0, 32.0, 43.0, 22.1], :phones_in_layer=>40, :ratio=>8.0}
